aeb263588d46e99de10b0371119aaa8fcf70a275
agentsimar <gursimarsingh1995@gmail.com>
2017-09-25 19:13:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/akal/sampleproject/activity/LoginActivity.java
@@ -0,0 +1,55 @@
+package com.example.akal.sampleproject.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.akal.sampleproject.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @InjectView(R.id.editTextEmail)   //using butterknife and declaring views
+    EditText eTxtEmail;
+
+    @InjectView(R.id.editTextPassword)
+    EditText eTxtPassword;
+
+    @InjectView(R.id.buttonLogin)
+    Button btnLogin;
+
+    @InjectView(R.id.textViewNewUser)
+    TextView txtRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.inject(this);
+
+        btnLogin.setOnClickListener(this);
+        txtRegister.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buttonLogin:
+
+                break;
+
+            case R.id.textViewNewUser:
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/akal/sampleproject/model/Util.java
@@ -0,0 +1,11 @@
+package com.example.akal.sampleproject.model;
+
+/**
+ * Created by Akal on 22-09-2017.
+ */
+
+public class Util {
+    public static final String URI = "https://gursimar.000webhostapp.com/id2882145_enc2017/";
+    public static final String REGISTER_ENDPOINT = "https://gursimar.000webhostapp.com/id2882145_enc2017/registeruser.php";
+
+}
