7ce67394da2e7561949fa9bb4b9104608851c6a4
agentsimar <gursimarsingh1995@gmail.com>
2017-10-03 18:38:13 +0530

    retrieve

--- app/src/main/java/com/example/akal/sampleproject/activity/HomeActivity.java
+++ app/src/main/java/com/example/akal/sampleproject/activity/HomeActivity.java
@@ -1,10 +1,15 @@
 package com.example.akal.sampleproject.activity;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -20,12 +25,14 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 //retrieve using volley
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     RequestQueue requestQueue;
     StringRequest stringRequest;
@@ -35,6 +42,8 @@ public class HomeActivity extends AppCompatActivity {
     ListView listView;
 
     UserAdapter userAdapter;
+    User user;
+    int pos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +86,7 @@ public class HomeActivity extends AppCompatActivity {
                         }
                         userAdapter = new UserAdapter(HomeActivity.this,R.layout.listitem,userList);
                         listView.setAdapter(userAdapter);
+                        listView.setOnItemClickListener(HomeActivity.this);
 
                     }else{
                         Toast.makeText(HomeActivity.this,message,Toast.LENGTH_LONG).show();
@@ -99,5 +109,100 @@ public class HomeActivity extends AppCompatActivity {
         });
 
         requestQueue.add(stringRequest);
+    }
+
+    void showUser(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(user.getName()+"Details");
+        builder.setMessage(user.toString());
+        builder.setPositiveButton("Done",null);
+        builder.create().show();
+    }
+    void deleteUser(){
+
+        stringRequest = new StringRequest(Request.Method.POST, Util.DELETE_ENDPOINT, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try{
+                    JSONObject jsonObject = new JSONObject(response);
+                    int success = jsonObject.getInt("success");
+                    String message = jsonObject.getString("message");
+                    if(success == 1){
+                        Toast.makeText(HomeActivity.this,message,Toast.LENGTH_LONG).show();
+                        userList.remove(pos);
+                        userAdapter.notifyDataSetChanged();
+                    }
+                }catch (Exception e){
+                    Toast.makeText(HomeActivity.this,"Some Exception: "+e,Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Toast.makeText(HomeActivity.this,"Some Error: "+error,Toast.LENGTH_LONG).show();
+
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> map = new HashMap<>();
+                map.put("id",String.valueOf(user.getUid()));
+                return map;
+            }
+        };
+        requestQueue.add(stringRequest);
+
+    }
+
+    void askForDeletion(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete "+user.getName());
+        builder.setMessage("Are you sure about this?");
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                deleteUser();
+            }
+        });
+        builder.setNegativeButton("cancel",null);
+        builder.create().show();
+
+    }
+
+    void showOptions(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        String[] items = {"View","Delete","Update"};
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                switch(i){
+                    case 0:
+                        showUser();
+                        break;
+                    case 1:
+                        askForDeletion();
+                        break;
+                    case 2:
+                }
+            }
+        });
+        builder.create().show();
+
+    }
+
+
+
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+        user = userList.get(i);
+        pos = i;
+        showOptions();
+
     }
 }
--- app/src/main/java/com/example/akal/sampleproject/model/Util.java
+++ app/src/main/java/com/example/akal/sampleproject/model/Util.java
@@ -9,6 +9,7 @@ public class Util {
     public static final String REGISTER_ENDPOINT = "https://gursimar.000webhostapp.com/id2882145_enc2017/registeruser.php";
     public static final String LOGIN_ENDPOINT = "https://gursimar.000webhostapp.com/id2882145_enc2017/loginuser.php";
     public static final String RETRIEVE_ENDPOINT = "https://gursimar.000webhostapp.com/id2882145_enc2017/retrieve.php";
+    public static final String DELETE_ENDPOINT = "https://gursimar.000webhostapp.com/id2882145_enc2017/deleteuser.php";
 
     public static final String PREFS_NAME = "sampleproject";
     public static final String KEY_LOGREG = "keyLoginRegister";
